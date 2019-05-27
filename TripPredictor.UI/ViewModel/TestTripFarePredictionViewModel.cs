2eabd214efbb231ab8f5f667b8000db195a7bdb4
Sabuj Saha <sabujece@gmail.com>
2019-05-27 09:40:06 -0500

    File renamed

--- TripPredictor.UI/ViewModel/TestTripFarePredictionViewModel.cs
+++ TripPredictor.UI/ViewModel/TestTripFarePredictionViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TripPredictor.Common;
 
 namespace TripPredictor.UI.ViewModel
 {
-    public class TestTripFarePredictionViewModel
+    public class TestTripFarePredictionViewModel:ViewModelBase,ITestTripDataPredictionViewModel
     {
+        public string PredictedResult { get; set; }
+        public TripData TestTripData { get; set; }
     }
 }
