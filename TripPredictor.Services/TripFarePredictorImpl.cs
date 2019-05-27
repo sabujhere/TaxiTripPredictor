4b47d7118803222d2c5b7bb9dcd463819972c7a3
Sabuj Saha <sabujece@gmail.com>
2019-05-27 01:06:58 -0500

    added TripFarePredictorImpl

---
+++ TripPredictor.Services/TripFarePredictorImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripPredictor.Common;
+
+namespace TripPredictor.Services
+{
+    public class TripFarePredictorImpl:ITripPredictor
+    {
+        public Task<bool> LoadTrainingDataAsync(string trainingDataFileName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EvaluationMetric GetEvaluationMetric(string testDataFileName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetPredictedResult(TripData tripData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
